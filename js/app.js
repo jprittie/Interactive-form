515b22db36b24f44eb7268d71eabd98cc28db6ce
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-06-10 18:26:58 -0400

    Activities registration updated

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 
 //2. When the page loads, give focus to the first text field
-$(document).ready(function(){
+$(function(){
   $("#name").focus();
 });
 
@@ -51,3 +51,45 @@ $("#design").change(function(){
 /* 5. Register for Activities section
 Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
 When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.*/
+$(":checkbox").change(function(){
+    if ($("input[name='js-frameworks']").is(":checked")) {
+      $("#conflict").remove();
+      // could gray-out colour of workshop label
+      $("input[name='express']").attr("disabled", true);
+      $("input[name='express']").parent().after("<p id='conflict' class='conflict'>This workshop conflicts with your current selection.</p>");
+    } else {
+      $("#conflict").remove();
+      $("input[name='express']").attr("disabled", false);
+    }
+
+   if ($("input[name='express']").is(":checked")) {
+      $("#js-frameworks").remove();
+      $("input[name='js-frameworks']").attr("disabled", true);
+      $("input[name='js-frameworks']").parent().after("<p id='js-frameworks' class='conflict'>This workshop conflicts with your current selection.</p>");
+    } else {
+      $("#js-frameworks").remove();
+      $("input[name='js-frameworks']").attr("disabled", false);
+    }
+
+    if ($("input[name='js-libs']").is(":checked")) {
+      $("#node").remove();
+      $("input[name='node']").attr("disabled", true);
+      $("input[name='node']").parent().after("<p id='node' class='conflict'>This workshop conflicts with your current selection.</p>");
+    } else {
+      $("#node").remove();
+      $("input[name='node']").attr("disabled", false);
+    }
+
+    if ($("input[name='node']").is(":checked")) {
+      $("#js-libs").remove();
+      $("input[name='js-libs']").attr("disabled", true);
+      $("input[name='js-libs']").parent().after("<p id='js-libs' class='conflict'>This workshop conflicts with your current selection.</p>");
+    } else {
+      $("#js-libs").remove();
+      $("input[name='js-libs']").attr("disabled", false);
+    }
+
+})
+
+/* 6. Cost of actvities
+As a user selects activities to register for, a running total is listed below the list of checkboxes. For example, if the user selects "Main conference" then Total: $200 should appear. If they add 1 workshop the total should change to Total: $300. */
