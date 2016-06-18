a49c556a5ff6ffcc82f170ddc867c3f73096d1e5
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-06-17 21:25:26 -0400

    Form validation updated

--- js/app.js
+++ js/app.js
@@ -1,98 +1,110 @@
 //1. Initialize global variables
-var total = 0;
+var total;
+var activitycounter;
 
 //2. When the page loads, give focus to the first text field
 $(function(){
   $("#name").focus();
 });
 
-/* 3. Job Role section:
-When "Other" option is selected, add text input field with id "other-title"
-and placeholder text "Your Title" */
+// 3. Job Role section:
+
+// First, hide "Other" input field if Javascript is enabled
+$("#other-title").hide();
+$("#title-label").hide();
+
+// If JS enabled and "Other" option selected, add text input field with id "other-title"
+// and placeholder text "Your Title"
 $("#title").change(function(){
     if ($("#title").val() == "other") {
-      $("#title").after($("<input type='text' id='other-title' placeholder='Your Title'>"));
+      $("#other-title").show();
     } else {
-      $("#other-title").remove();
+      $("#other-title").hide();
     }
 });
 
-/* 4. T-Shirt Info section:
-For T-Shirt color menu, only display the options that match the design selected in the "Design" menu.
-If the user selects "Theme - JS Puns" then the color menu
-should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
-If the user selects "Theme - I ♥ JS" then the color menu
-should only display "Tomato," "Steel Blue," and "Dim Grey."
-*/
-// Should I disable colour selection until theme is chosen?
-// Make initial text of Colour dropdown "Select Colour"
-$("option[value='cornflowerblue']").before($("<option selected>Select Colour</option>"));
 
-$("#design").change(function(){
-    if ($("#design").val() == "js puns") {
+// 4. T-Shirt Info section
+// Only display colours that match the selected design
+
+    // 4.1 NOT NEEDED ON ACCOUNT OF EE
+    // If no design selected, colour options don't appear,
+    // and colour menu reads “Please select a T-shirt theme”
+
+    /*if ($("#design").val() == "select theme") {
         // clear current colour dropdown
         $("#color").children().remove();
-        // add available colours
-        $("#color").append($("<option selected>Select Colour</option>"));
+        $("#color").append($("<option>Please select a T-shirt theme</option>"));
+    }*/
+
+// 4.2 EE: Hide "Color" label and select menu until a T-Shirt design is selected
+$("#colors-js-puns").hide();
+
+$("#design").change(function(){
+    // First, clear current colour dropdown
+    $("#color").children().remove();
+    // Then show colours based on design selected
+    if ($("#design").val() == "js puns") {
         $("#color").append($("<option value='cornflowerblue'>Cornflower Blue</option>"));
         $("#color").append($("<option value='darkslategrey'>Dark Slate Grey</option>"));
         $("#color").append($("<option value='gold'>Gold</option>"));
-    } else {
-        // clear current colour dropdown
-        $("#color").children().remove();
-        // add available colours
-        $("#color").append($("<option selected>Select Colour</option>"));
+        $("#colors-js-puns").show();
+    } else if ($("#design").val() == "heart js") {
         $("#color").append($("<option value='tomato'>Tomato</option>"));
         $("#color").append($("<option value='steelblue'>Blue Steel</option>"));
         $("#color").append($("<option value='dimgrey'>Dim Grey</option>"));
+        $("#colors-js-puns").show();
+    } else {
+        $("#colors-js-puns").hide();
     }
 })
 
 /* 5. Register for Activities section
-Some events are at the same time as others. If the user selects a workshop, don't allow selection of a workshop at the same date and time -- you should disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
-When a user unchecks an activity, make sure that competing activities (if there are any) are no longer disabled.*/
+Don't allow selection of two workshops at the same date and time.
+Disable checkbox and indicate that workshop in competing time slot isn't available.
+When workshop is unchecked, any competing activites are enabled again */
 $(":checkbox").change(function(){
+
+  // First, clear any existing messages about workshop conflicts
+  $("#express").remove();
+  $("#js-frameworks").remove();
+  $("#node").remove();
+  $("#js-libs").remove();
+
+  // Next, prevent schedule conflicts and add messages
     if ($("input[name='js-frameworks']").is(":checked")) {
-      $("#express").remove();
-      // could gray-out colour of workshop label
       $("input[name='express']").attr("disabled", true);
       $("input[name='express']").parent().after("<p id='express' class='conflict'>This workshop conflicts with your current selection.</p>");
+      // Gray out colour of workshop label
       $("input[name='express']").parent().css('color','gray');
     } else {
-      $("#express").remove();
       $("input[name='express']").attr("disabled", false);
       $("input[name='express']").parent().css('color','#000');
     }
 
    if ($("input[name='express']").is(":checked")) {
-      $("#js-frameworks").remove();
       $("input[name='js-frameworks']").attr("disabled", true);
       $("input[name='js-frameworks']").parent().after("<p id='js-frameworks' class='conflict'>This workshop conflicts with your current selection.</p>");
       $("input[name='js-frameworks']").parent().css('color','gray');
     } else {
-      $("#js-frameworks").remove();
       $("input[name='js-frameworks']").attr("disabled", false);
       $("input[name='js-frameworks']").parent().css('color','#000');
     }
 
     if ($("input[name='js-libs']").is(":checked")) {
-      $("#node").remove();
       $("input[name='node']").attr("disabled", true);
       $("input[name='node']").parent().after("<p id='node' class='conflict'>This workshop conflicts with your current selection.</p>");
       $("input[name='node']").parent().css('color','gray');
     } else {
-      $("#node").remove();
       $("input[name='node']").attr("disabled", false);
       $("input[name='node']").parent().css('color','#000');
     }
 
     if ($("input[name='node']").is(":checked")) {
-      $("#js-libs").remove();
       $("input[name='js-libs']").attr("disabled", true);
       $("input[name='js-libs']").parent().after("<p id='js-libs' class='conflict'>This workshop conflicts with your current selection.</p>");
       $("input[name='js-libs']").parent().css('color','gray');
     } else {
-      $("#js-libs").remove();
       $("input[name='js-libs']").attr("disabled", false);
       $("input[name='js-libs']").parent().css('color','#000');
     }
@@ -103,6 +115,9 @@ $(":checkbox").change(function(){
 As a user selects activities to register for, a running total is listed below the list of checkboxes. For example, if the user selects "Main conference" then Total: $200 should appear. If they add 1 workshop the total should change to Total: $300. */
 
 $(":checkbox").change(function(){
+    enableButton();
+    $("#activityerror").remove();
+
     var total = 0;
     $("#cost").remove();
 
@@ -121,92 +136,126 @@ $(":checkbox").change(function(){
     }
 })
 
-/* 7. Payment Info section
-Display payment sections based on chosen payment option */
+// 7. Payment Info section
 
-$("option[value='credit card']").prop("selected", true);
-
+// First, make credit card the default option,
+// This can be done just by removing the "Select Payment Method" option
+// and hide the "Select method" option, because we don't need it anymore
+//$("option[value='credit card']").prop("selected", true);
+$("option[value='select_method']").remove();
+// Then, hide payment information for the other two options
+$("#paypal").hide();
+$("#bitcoin").hide();
+// Next, display payment sections based on chosen payment option
 $("#payment").change(function(){
-        $(this).val() === "credit card" ? $("#credit-card").show() : $("#credit-card").hide();
-        $(this).val() === "paypal" ? $("#paypal").show() : $("#paypal").hide();
-        $(this).val() === "bitcoin" ? $("#bitcoin").show() : $("#bitcoin").hide();
+    $(this).val() === "credit card" ? $("#credit-card").show() : $("#credit-card").hide();
+    $(this).val() === "paypal" ? $("#paypal").show() : $("#paypal").hide();
+    $(this).val() === "bitcoin" ? $("#bitcoin").show() : $("#bitcoin").hide();
 });
+
 
 
 /* 8. Form validation. Display error messages and don't let the user submit
 the form if any of these validation errors exist: */
 
 $("button[type='submit']").on("click", function(){
-
-  // 8.1 Name field can't be empty
-  if ($("#name").val() === "") {
-    $("button[type='submit']").attr("disabled", true);
-    $("#name").focus().attr("placeholder","Please enter your name");
-  }
-
-  // 8.2 At least one activity must be checked from the list under "Register for Actitivities."
-  if ($(".activities input").prop("checked", false)) {
-    $("button[type='submit']").attr("disabled", true);
-    // $(".activities").focus();
-    $(".activities legend").after("<p id='activityerror' class='errortext'>Please select an activity.</p>");
-    // $("#activityerror").focus();
-    // location.href = $("#activityerror")
-    // $("input[name='all]").focus();
-    // Need error message
-    // But also, focus doesn't work on class
-    // Do I need to remove activityerror?
-  }
-  });
-
-/* // 8.3 Payment option must be selected
-  $("#payment").change(function(){
-
-    $("#paymenterror").remove();
-
-    if ($("#payment").val() === "select_method") {
-      $("button[type='submit']").attr("disabled", true);
-      $("#payment").after("<p id='paymenterror'>Please select a payment method.</p>");
-    }
-  }); */
-
-  //Activites error doesn't register anymore; must set button back to submit enabled?
-
-
-
-$("button[type='submit']").on("click", function(){
-
-  // 8.3 Payment option must be selected
-  $("#paymenterror").remove();
-  if ($("#payment").val() === "select_method") {
-    $("button[type='submit']").attr("disabled", true);
-    $("#payment").after("<p id='paymenterror' class='errortext'>Please select a payment method.</p>");
-  }
-
-  // 8.4 Credit card details
-  /*If "Credit card" is the selected payment option, make sure the user supplied
-  a credit card number, a zip code, and a 3 number CVV value.*/
+    // First, clear any existing error messages
+    //$("#activityerror").remove();
     $("#paydetailserror").remove();
-    if ($("#payment").val() === "credit card" && $("#cc-num").val() === "" || $("#zip").val() === "" || $("#cvv").val() === "" ) {
-        $("button[type='submit']").attr("disabled", true);
-        $("#payment").after("<p id='paydetailserror' class='errortext'>Please complete your payment details.</p>");
+    // Next, use variables to track problems with input;
+    // these will tell us at end of function whether form can be sumbitted
+    var submitcounter = 0;
+    var activitycounter = 0;
+    // Then, make sure button is enabled, in case it has previously been disabled
+    $("button[type='submit']").attr("disabled", false);
+
+
+    // 8.1 Name field can't be empty
+    if ($("#name").val() === "") {
+      submitcounter += 1;
+      $("#name").focus().attr("placeholder","Please enter your name");
+      $("#name").addClass("errortext");
     }
+
+    // 8.2 At least one activity must be checked from the list under "Register for Actitivities."
+      /*$(".activities input").each(function(){
+        if ($(this).prop("checked", false)) {
+          console.log("This isn't checked.")
+          activitycounter += 1;
+        }
+        return activitycounter;
+      })
+
+      if ($(".activities input").length === activitycounter){
+        submitcounter += 1;
+        $(".activities").after("<p id='activityerror' class='errortext'>Please select an activity.</p>");
+      }*/
+
+      $(".activities input").each(function(){
+        if ($(this).is(":checked")) {
+          console.log("This is checked.")
+          activitycounter += 1;
+        }
+        return activitycounter;
+      })
+
+      if (activitycounter == 0){
+        submitcounter += 1;
+        $(".activities").after("<p id='activityerror' class='errortext'>Please select an activity.</p>");
+      }
+
+
+
+    // 8.3 Payment option must be selected
+    // This can be done just by removing the "Select Payment Method" option
+    // (which makes sense anyway, since Credit Card must be the default)
+    $("option[value='select_method']").remove();
+
+
+
+    // 8.4 Credit card details
+    /*If "Credit card" is the selected payment option, make sure the user supplied
+    a credit card number, a zip code, and a 3 number CVV value.*/
+      if ($("#payment").val() == "credit card" && ($("#cc-num").val() == "" || $("#zip").val() == "" || $("#cvv").val() === "") ) {
+          console.log("Credit card fields are blank.")
+          submitcounter += 1;
+          $("#payment").after("<p id='paydetailserror' class='errortext'>Please complete your payment details.</p>");
+      }
+
+
+    // 8.4 Email field must be a validly formatted e-mail address
+    //***WHY DID THIS WORK WHEN I HIT THE ENTER KEY ON THE FIELD?
+      var emailinput = $("#mail").val();
+      var emailformula = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+
+      if (!emailformula.test(emailinput)) {
+        submitcounter += 1;
+        $("#mail").attr("placeholder","Please enter a valid email");
+        $("#mail").addClass("errortext");
+        //$("#mail").after("<p id='emailerror' class='errortext'>Please enter a valid email.</p>");
+      }
+
+    // 8.5 Credit card number must be valid
+    /* if ($("option[value='credit card']").is(":selected")){
+        var ccinput = $("#cc-num").val();
+        var ccformula =
+    } */
+
+
+  if (submitcounter > 0) {
+  $("button[type='submit']").attr("disabled", true);
+  }
+
 });
-//*If you change payment type after you give an error, error message should be removed
+
+
+function enableButton(){
+  $("button[type='submit']").attr("disabled", false);
+}
+
+// IF SUBMIT BUTTON WORKS, FORM CLEARS; IS THAT THE RIGHT THING TO DO?
+
+// HITTING ENTER ON ANY INPUT SUBMITS FORM - MUST DISABLE THIS
 // At one point, I saw bitcoin and paypal info show when they shouldn't have
 
-
-// 8.4 Email field must be a validly formatted e-mail address
-// Should I do this on keyup, or on submit?
-//***WHY DID THIS WORK WHEN I HIT THE ENTER KEY ON THE FIELD?
-$("button[type='submit']").on("click", function(){
-    $("#emailerror").remove();
-    var input = $("#mail").val();
-    var formula = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
-
-    if(!formula.test(input)) {
-      $("button[type='submit']").attr("disabled", true);
-      $("#mail").after("<p id='emailerror' class='errortext'>Please enter a valid email.</p>");
-    }
-});
-
-// Where do I re-enable submit button?
+// How do I jump to part of page where error is?
